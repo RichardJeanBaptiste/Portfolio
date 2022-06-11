5433ea2997ddac01916af3918fa88800385c5a98
Richard Jean-Baptiste <Richinbk1@gmail.com>
2022-06-11 16:08:10 -0400

    sadas

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import maleboy from './assets/male_boy.png';
 import Typed from 'react-typed';
 import PersistentDrawerLeft from './components/PersistantDrawer';
 import Typography from '@mui/material/Typography';
+import Button  from '@mui/material/Button';
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 // import Drawer from '@mui/material/Drawer';
 // import AppBar from '@mui/material/AppBar';
 // import Toolbar from '@mui/material/Toolbar';
@@ -41,35 +43,36 @@ function App() {
 
   const Styles = {
     root: {
-      display:'flex',
       width:'100vw',
       height:'100vh',
       backgroundColor: '#353353'
     },
     titleStyle: {
-      position: 'absolute',
-      left: '50%',
-      top: '50%',
-      transform: 'translate(-50%, -50%)',
-      height: { xs: '22vh', sm: '22vh', md:'32vh', lg: '24vh', xl: '35vh'},
-      width: { xs: '67vw', sm: '43vw', md: '32vw', lg: '24vw', xl: '25vw'},
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      marginTop: '7vh',
+      width: '90vw',
+      height:'90vh',
     },
     avatarStyle: {
-      margin: 'auto',
-      width: '50%',
-      height: '70%',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      marginTop: '10em',
+      width: '10%',
+      height: '20%',
     },
     subTitleText1: {
       color: 'whitesmoke',
-      fontSize: {  xs:'24px', sm:'30px', lg: '32px'},
+      fontSize: {  xs:'24px', sm:'30px', lg: '30px'},
       fontFamily: 'Rubik',
       fontWeight: 'bold',
-      marginTop: '10px'
+      marginTop: '.8em',
     },
     subTitleText2: {
       color: 'whitesmoke',
       fontSize: '24px',
       whiteSpace: 'nowrap',
+      marginTop: '.8em',
     },
     typedStyle: {
       //marginLeft: '-22px',
@@ -77,33 +80,63 @@ function App() {
       fontSize: '16px',
       fontFamily: 'Rubik',
     },
+    hireMeButton: {
+      borderRadius: '30px',
+      width: '9em',
+      height: '3em',
+      
+    },
     aboutmeContainer: {
       width:'100vw',
       height:'100vh',
     }
   }
  
+/**
+ * <Box sx={{ marginTop: {xs: '66px', sm: '77px', lg:'91px'}, marginLeft: {xs: '75px', sm: '97px', lg:'163px'} }}>
+ * 
+ * 
+ *  position: 'absolute',
+      left: '50%',
+      top: '40%',
+      transform: 'translate(-50%, -50%)',
+      height: { xs: '22vh', sm: '22vh', md:'32vh', lg: '24vh', xl: '35vh'},
+      width: { xs: '67vw', sm: '43vw', md: '32vw', lg: '24vw', xl: '25vw'},
+ * 
+ * 
+ * 
+ * 
+            
+            
+            
+            <Button variant='contained' size='large' sx={{ borderRadius: '30px', width: {xs: '50%', lg: '27%'}, marginTop: {xs: '58px', sm:'58px'}, marginLeft: {xs: '64px', sm: '17vw', md:'91px', lg: '9.5vw'} }}>Hire Me</Button>
+ * 
+ */
+
   return (
 
-    <Box > 
+    <Box> 
       <Box sx={Styles.root}>
 
         <PersistentDrawerLeft/>
-      
+
         {/** Avatar */}
         <Box sx={Styles.titleStyle}>
-          <Avatar alt="Richard Jean-Baptiste" src={maleboy} sx={Styles.avatarStyle}/>
+          <Avatar alt="Richard Jean-Baptiste" src={maleboy} sx={Styles.avatarStyle}/> 
           <Typography variant='h4' component='h4' textAlign='center' sx={Styles.subTitleText1}>Hi, Im Richard</Typography>
           <Typography varaiant='h4' component='h4' textAlign='center' sx={Styles.subTitleText2}>
-              I'm a <Typed
-                      style={{ marginTop: '10px', color: 'whitesmoke', fontSize: '24px', fontFamily: 'Rubik'}}
-                      strings={['Software Developer','Front-End Delevoper', 'Backend Developer']}
-                      typeSpeed={40}
-                      backSpeed={50}
-                      loop
-                    />
+                I'm a <Typed
+                        style={{ marginTop: '10px', color: 'whitesmoke', fontSize: '24px', fontFamily: 'Rubik'}}
+                        strings={['Software Developer','Front-End Delevoper', 'Backend Developer']}
+                        typeSpeed={40}
+                        backSpeed={50}
+                        loop
+                      />
           </Typography>
-          
+          <Box sx={{ width: '8em', height: '3em', marginLeft: 'auto', marginRight: 'auto', marginTop: '7.9em'}}>
+            <Button variant='contained' size='medium' sx={Styles.hireMeButton}>Hire Me</Button>
+            <KeyboardDoubleArrowDownIcon  id='scrollButton' sx={{ marginLeft: '42%', marginTop: '7.2em',color: 'white'}}/>
+          </Box>
         </Box>
       </Box>
 
--- src/components/PersistantDrawer.js
+++ src/components/PersistantDrawer.js
@@ -1,3 +1,4 @@
+import '../App.css';
 import React, {useState } from 'react';
 import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
@@ -25,26 +26,7 @@ import { Typography } from '@mui/material';
 
 const drawerWidth = 180;
 
-/*
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: `-${drawerWidth}px`,
-    ...(open && {
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginLeft: 0,
-    }),
-  }),
-);
-*/
+
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
@@ -98,7 +80,7 @@ export default function PersistentDrawerLeft() {
     return (
         <ListItem disablePadding>
             <ListItemButton sx={{ color: iconColor }}>
-                <ListItemIcon sx={{color: iconColor}} onMouseOver={activeColor} onMouseLeave={nonActiveColor} onTouchStart={activeColor} onTouchEnd={nonActiveColor}>
+                <ListItemIcon id='List1' sx={{color: iconColor}} onMouseOver={activeColor} onMouseLeave={nonActiveColor}>
                     {props.ICON}
                     <ListItemText primary={<Typography sx={{ fontFamily:'Rubik' }} >{props.text}</Typography>} sx={{ marginLeft: '25px'}} />
                 </ListItemIcon>
